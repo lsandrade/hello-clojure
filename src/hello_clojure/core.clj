ec2e1650020e70245730ef78fb74e2e7788a79be
Luan Andrade <luan.andrade@thoughtworks.com>
2020-02-24 14:43:59 -0300

    cria metodo para verificar se precisa aplicar desconto

--- src/hello_clojure/core.clj
+++ src/hello_clojure/core.clj
@@ -7,4 +7,12 @@
   (let [valor-descontado (* valor-bruto taxa-desconto)]
   (- valor-bruto valor-descontado))))
 
+(defn aplica-desconto?
+  [valor]
+  ( if (> valor 100)
+  true
+  false))
+
+(aplica-desconto? 101)
+
 (calcula-desconto 100)
