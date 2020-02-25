b52a776264f4f6207dc34b048d0174b2ca424751
Luan Andrade <luan.andrade@thoughtworks.com>
2020-02-24 21:08:31 -0300

    retorna buzz

--- src/fizzbuzz/core.clj
+++ src/fizzbuzz/core.clj
@@ -1,8 +1,12 @@
 (ns fizzbuzz.core)
 
 (defn fizzbuzz [numero]
-  (str "fizz"))
+  (if (= 0 (mod 3 numero))
+    (str "fizz")
+    (str "buzz")))
 
 ; deve retornar fizz se numero for divisivel por 3
 (= (fizzbuzz 3) "fizz")
 
+; deve retornar buzz se numero for divisivel por 5
+(= (fizzbuzz 5) "buzz")
