f0f28c36e8e0626b3d8570450f5bff8aeb08168c
Luan Andrade <luansandrade92@hotmail.com>
2020-02-26 21:55:22 -0300

    recupera elementos de um mapa

--- src/hello_clojure/controle_financeiro.clj
+++ src/hello_clojure/controle_financeiro.clj
@@ -5,3 +5,10 @@
 
 ; pegar elemento do mapa
 (get transacao :valor)
+(:tipo transacao)
+
+;retorna nil quando chave nao existe
+(:nao-tem transacao)
+
+; retorna {} como default para chave que nao tem
+(:nao-tem transacao {})
