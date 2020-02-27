124642cb86a3a0f88dbe333830faf095b1e7809b
Luan Andrade <luansandrade92@hotmail.com>
2020-02-26 22:06:14 -0300

    retorna resumo de transacoes

--- src/hello_clojure/controle_financeiro.clj
+++ src/hello_clojure/controle_financeiro.clj
@@ -12,3 +12,13 @@
 
 ; retorna {} como default para chave que nao tem
 (:nao-tem transacao {})
+
+(defn resumo [transacao]
+  (select-keys transacao [:valor :tipo :data]))
+
+(def transacoes [
+                 {:valor 33.0 :tipo "despesa" :comentario "almoço" :data "01/02/2020"}
+                 {:valor 1000.0 :tipo "receita" :comentario "freela" :data "02/02/2020"}
+                 {:valor 26.0 :tipo "despesa" :comentario "livro de clojure" :data "03/02/2020"}])
+
+(map resumo transacoes)
