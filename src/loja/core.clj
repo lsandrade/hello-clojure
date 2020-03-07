3397baa2ecd902c86049e7257e97fbd50f44d410
Luan Andrade <luansandrade92@hotmail.com>
2020-03-07 16:27:01 -0300

    Realiza operacoes em pedidos

--- src/loja/core.clj
+++ src/loja/core.clj
@@ -1,4 +1,5 @@
-(ns loja.core)
+(ns loja.core
+  (:require [loja.db :as db]))
 
 (def nomes, ["Harry", "Rony", "Hermione"])
 
@@ -36,3 +37,46 @@
 (conta nomes)
 
 (conta-loop nomes)
+
+(println (db/pedidos))
+(println (group-by :usuario (db/pedidos)))
+
+; conta usuarios
+(println (count (vals (group-by :usuario (db/pedidos)))))
+
+; conta pedidos por usuario
+(println (map count (vals (group-by :usuario (db/pedidos)))))
+
+(defn conta-total-por-usuario
+  [[usuario pedidos]]
+  {:usuario-id usuario
+   :total-pedidos (count pedidos)})
+
+(->> (db/pedidos)
+     (group-by :usuario)
+     (map conta-total-por-usuario)
+     println)
+
+(defn total-do-item
+  [[item-id detalhes]]
+  (* (get detalhes :quantidade 0) (get detalhes :preco 0)))
+
+(defn total-do-pedido
+  [pedido]
+  (reduce + (map total-do-item pedido)))
+
+(defn conta-total-por-pedido [pedidos]
+  (->> pedidos
+       (map :itens)
+       (map total-do-pedido)
+       (reduce +)))
+
+(defn total-gasto-por-usuario
+  [[usuario pedidos]]
+  {:usuario-id usuario
+   :total-gasto (conta-total-por-pedido pedidos)})
+
+(->> (db/pedidos)
+     (group-by :usuario)
+     (map total-gasto-por-usuario)
+     println)
